ddf1f4b4ced5ec6de16a232404522084e0a5f096
Alessandro Liuzzi <61493515+alessandroliuzzi@users.noreply.github.com>
2022-04-06 20:55:28 +0200

    Add files via upload

---
+++ clustering.py
@@ -0,0 +1,68 @@
+from os import path
+from sys import argv
+from sklearn.cluster import KMeans
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn import metrics
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+df = pd.read_csv('dataset/data.csv')
+
+df.head()
+
+df.describe()
+
+
+
+columns = df.columns.to_list()
+
+
+
+columns.remove('label')
+
+columns.remove('filename')
+
+
+X = df[columns]
+
+Y = df['label']
+
+X.head()
+
+Y.head()
+
+minmax= MinMaxScaler()
+
+scaledX = pd.DataFrame(minmax.fit_transform(X), columns=X.columns.to_list())
+
+scaledX.head()
+
+
+
+len(Y.unique())
+
+kmeans=KMeans(n_clusters=len(Y.unique()), max_iter=3000, verbose=True)
+
+kmeans.fit(scaledX, Y)
+
+clusters = kmeans.labels_
+
+
+def purity_score(y_true, y_pred):
+
+    # compute contingency matrix (also called confusion matrix)
+
+    contingency_matrix = metrics.cluster.contingency_matrix(y_true, y_pred)
+
+    # return purityreturn np.sum(np.amax(contingency_matrix, axis=0)) / np.sum(contingency_matrix)
+
+    return np.sum(np.amax(contingency_matrix, axis=0)) / np.sum(contingency_matrix)
+
+metrics.cluster.contingency_matrix(Y, clusters)
+
+purity = purity_score(Y, clusters)
+print(purity)
+
+
